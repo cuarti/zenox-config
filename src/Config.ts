695fcf23b3c8317470fc8f6c1c3493e28c751608
cuarti <cuarti1992@gmail.com>
2017-07-26 23:11:25 +0200

    Rebuild ConfigBuilder and add comments

--- src/Config.ts
+++ src/Config.ts
@@ -1,22 +1,45 @@
 
 
+/**
+ * Config data
+ *
+ * @todo    Abstract it in @agama/types as AssignableObject<T = any> for assign new properties to object
+ */
 export interface ConfigData {
     [key: string]: any;
 }
 
+/**
+ * Config set
+ */
 export class Config {
 
     private data: ConfigData;
 
+    /**
+     * Constructor method
+     *
+     * @param   data    Config data
+     */
     public constructor(data: ConfigData) {
         this.data = data;
     }
 
-    public get<T>(): T {
-
-        return null;
+    /**
+     * Get config value by namespace
+     *
+     * @param   namespace   Config namespace
+     * @returns             Config value
+     */
+    public get<T>(namespace: string): T {
+        return namespace.split('.').reduce((r, k) => r[k], this.data);
     }
 
+    /**
+     * Get all config values
+     *
+     * @returns {ConfigData}
+     */
     public getAll(): ConfigData {
         return this.data;
     }
--- src/ConfigBuilder.ts
+++ src/ConfigBuilder.ts
@@ -5,19 +5,78 @@ import {join} from 'path';
 import {Config, ConfigData} from './Config';
 
 
+/**
+ * Builder for config
+ */
 export class ConfigBuilder {
 
-    public build(path: string, environment: string): Promise<Config> {
+    private path: string;
+    private environment: string;
 
-        let envPath = join(path, environment.toLowerCase());
+    /**
+     * Constructor method
+     *
+     * @param   path        Base config path
+     * @param   environment Environment value
+     */
+    public constructor(path: string, environment?: string) {
+        this.path = path;
+        this.environment = environment;
+    }
+
+    /**
+     * Build config object
+     *
+     * @returns Config set
+     */
+    public build(): Promise<Config> {
+
+        let envPath = join(this.path, this.environment.toLowerCase());
 
         return Promise.all([
-            this.readFolder(path).catch(() => Promise.resolve({})),
-            this.readFolder(envPath).catch(() => Promise.resolve({}))
+            this.readFolder(this.path),
+            this.readFolder(envPath)
         ]).then<Config>(([data, envData]) => new Config(this.merge(data, envData)));
 
     }
 
+    /**
+     * Get config data from folder
+     *
+     * @param   path    Folder path
+     * @returns         Config stored in folder files
+     */
+    private readFolder(path: string): Promise<ConfigData> {
+
+        return new Promise(resolve => {
+            readdir(path, (err, files) => {
+                if(err) {
+                    resolve({});
+                } else {
+                    resolve(files.reduce((r, f) => {
+
+                        if(f.endsWith('.js')) {
+
+                            let name = f.substring(0, f.length - 3);
+                            r[name] = require(join(path, f));
+                        }
+
+                        return r;
+                    }, {}));
+                }
+            });
+        });
+
+    }
+
+    /**
+     * Merge two objects in new one
+     *
+     * @param   obj1    First object to merge
+     * @param   obj2    Second object to merge
+     * @returns         Merged object
+     * @todo            Abstract it in @agama/types
+     */
     private merge(obj1: Object = {}, obj2: Object = {}): Object {
 
         let keys = Object.keys(obj1).concat(Object.keys(obj2))
@@ -35,39 +94,8 @@ export class ConfigBuilder {
                 r[k] = obj2[k];
             }
 
-            // if(obj2.hasOwnProperty(k)) {
-            //     r[k] = typeof obj2[k] === 'object' ? this.merge()
-            // } else {
-            //     r[k] = obj1[k];
-            // }
-
-            // r[k] = obj2.hasOwnProperty(k) ? obj2[k] : obj1[k];
-
             return r;
         }, {});
-
-    }
-
-    private readFolder(path: string): Promise<ConfigData> {
-
-        return new Promise((resolve, reject) => {
-            readdir(path, (err, files) => {
-                if(err) {
-                    reject(err);
-                } else {
-                    resolve(files.reduce((r, f) => {
-
-                        if(f.endsWith('.js')) {
-
-                            let name = f.substring(0, f.length - 3);
-                            r[name] = require(join(path, f));
-                        }
-
-                        return r;
-                    }, {}));
-                }
-            });
-        });
 
     }
 
